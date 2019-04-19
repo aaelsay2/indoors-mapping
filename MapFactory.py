4e0037d28ea8f99dcdba812b62fb69590f530afb
aaelsay2 <aaelsay2@illinois.edu>
2019-04-19 11:35:35 -0500

    updated python files

--- MapFactory.py
+++ MapFactory.py
@@ -20,129 +20,158 @@ class MapFactory:
 
     def map1(self):
         map = Map()
-        map.set_boundary([1000,1000])
-        map.add_element(Element('1', 'column', [(0,0),(20,0),(20,20),(0,20)]))
-        map.add_element(Element('2', 'column', [(240,0),(260,0),(260,20),(240,20)]))
-        map.add_element(Element('3', 'column', [(480,0),(520,0),(520,20),(480,20)]))
-        map.add_element(Element('4', 'column', [(740,0),(760,0),(760,20),(740,20)]))
-        map.add_element(Element('5', 'column', [(980,0),(999,0),(999,20),(980,20)]))        
-        map.add_element(Element('6', 'column', [(240,240),(260,240),(260,260),(240,260)],True))
-        map.add_element(Element('7', 'column', [(480,240),(520,240),(520,260),(480,260)],True))
-        map.add_element(Element('8', 'column', [(740,240),(760,240),(760,260),(740,260)], True))
-        map.add_element(Element('9_1', 'wall', [(0,500),(200,500),(200,520),(0,520)]))
-        map.add_element(Element('9_2', 'wall', [(300,500),(500,500),(500,520),(300,520)]))
-        map.add_element(Element('10_1', 'wall', [(500,500),(700,500),(700,520),(500,520)],True))
-        map.add_element(Element('10_2', 'wall', [(800,500),(999,500),(999,520),(800,520)],True))
-        map.add_element(Element('11', 'column', [(240,800),(260,800),(260,820),(240,820)]))
-        map.add_element(Element('12', 'wall', [(490,520),(510,520),(510,999),(490,999)]))
-        map.add_element(Element('13', 'column', [(740,800),(760,800),(760,820),(740,820)],True))
-        map.add_element(Element('14', 'wall', [(0,960),(20,960),(20,999),(0,999)]))
-        map.add_element(Element('15', 'wall', [(980,960),(999,960),(999,999),(980,999)],True))
+        map.set_boundary([100,70])
+        map.add_element(Element('column1', [(0,0),(2,0),(2,4),(0,4)],False))
+        map.add_element(Element('column2', [(20,0),(24,0),(24,4),(20,4)],False))
+        map.add_element(Element('column3', [(40,0),(44,0),(44,4),(40,4)],False))
+        map.add_element(Element('column4', [(56,0),(60,0),(60,4),(56,4)],False))
+        map.add_element(Element('column5', [(76,0),(80,0),(80,4),(76,4)],False))
+        map.add_element(Element('column6', [(97,0),(99,0),(99,4),(97,4)],False))
+        map.add_element(Element('column7', [(0,20),(2,20),(2,24),(0,24)],False))
+        map.add_element(Element('column8', [(28,20),(32,20),(32,24),(28,24)],False))
+        map.add_element(Element('column9', [(48,20),(52,20),(52,24),(48,24)],False))
+        map.add_element(Element('column10', [(68,20),(72,20),(72,24),(68,24)],True))
+        map.add_element(Element('column11', [(97,20),(99,20),(99,24),(97,24)],True))
+        map.add_element(Element('wall12', [(0,44),(24,44),(24,46),(0,46)],False))
+        map.add_element(Element('wall13', [(40,44),(49,44),(49,46),(40,46)],False))
+        map.add_element(Element('wall14', [(51,44),(69,44),(69,46),(51,46)],True))
+        map.add_element(Element('wall15', [(76,44),(99,44),(99,46),(76,46)],True))
+        map.add_element(Element('wall16', [(49,44),(51,44),(51,69),(49,69)],False))
+        map.add_element(Element('column17', [(28,60),(32,60),(32,64),(28,64)],False))
+        map.add_element(Element('column18', [(68,60),(72,60),(72,64),(68,64)],True))
+        map.add_obstacle(Obstacle([(50,16),(64,16),(64,18),(50,18)]))
+        map.add_obstacle(Obstacle([(30,30),(34,30),(34,34),(30,34)]))
+        map.add_obstacle(Obstacle([(69,24),(74,24),(74,30),(69,30)]))
+        map.add_obstacle(Obstacle([(18,46),(20,46),(20,64),(18,64)]))
+        map.add_obstacle(Obstacle([(58,50),(62,50),(62,64),(58,64)]))
+        
         return map
 
 
 class Map:
     def __init__(self):
-        self.boundary = [1000,1000]
+        self.boundary = np.array([0,0])
         self.elements = []
+        self.obstacles = []
 
     def clone(self):
         map = Map()
         map.set_boundary(self.boundary)
         for el in self.elements:
-            new_el = Element(el.get_id() , el.get_category(), el.get_geometry(), el.is_inprogress())
+            new_el = Element(el.get_name(), el.get_geometry(), el.is_inprogress())
             map.add_element(new_el)
+
+        for ob in self.obstacles:
+            new_ob = Obstacle(ob.get_geometry())
+            map.add_obstacle(new_ob)
+
         return map
 
     def get_boundary(self):
-        return np.array(self.boundary)
+        return self.boundary
 
     def set_boundary(self,bound):
         if isinstance(bound, list) and len(bound) == 2:
-            self.boundary = bound
+            self.boundary = np.array(bound)
         else:
             raise TypeError('Boundary has to be a list of length 2')
 
     def add_element(self, element):
-        if isinstance(element, Element):
-            el, idx = self.get_element(element.get_id())
-            if idx == None:
-                self.elements.append(element) 
-        else:
-            raise TypeError('Added element is not of type Element')
-
-    def remove_element(self, id):
-        el, idx = self.get_element(id)
-        if not(idx == None):
-            self.elements[idx] = []
-        else:
-            raise TypeError('Element to be removed does not exist or duplicated')
-
-    def get_element(self,id):
-        indices = [i for (e, i) in zip(self.elements, range(len(self.elements))) if e.get_id() == id]
-        if len(indices) == 1:
-            return self.elements[indices[0]], indices[0]
-        else:
-            return None,None
+        new_index = 1
+        if len(self.elements)>0:
+            new_index = new_index + self.elements[-1].get_id()
+        element.set_id(new_index)
+        self.elements.append(element)
+    
+    def add_obstacle(self, obstacle):
+        new_index = 1
+        if len(self.obstacles)>0:
+            new_index = new_index + self.obstacles[-1].get_id()
+        obstacle.set_id(new_index)
+        self.obstacles.append(obstacle)
     
     def get_elements(self):
         return self.elements
 
-    def get_inprogress_ids(self):
-        return [el.get_id() for el in self.elements if el.is_inprogress()]
+    def get_obstacles(self):
+        return self.obstacles
 
-    def get_img(self):
-        boundary = self.get_boundary()
-        img = np.ones(boundary)
+    def get_inprogress(self):
+        return [el for el in self.elements if el.is_inprogress()]
+
+    def get_element_index(self, element):
+        indices = [idx for i,el in enumerate(self.elements) if element.get_id() == el.get_id()]
+        if len(indices)>0:
+            return indices[0]
+        return None
+
+    def get_obstacle_index(self, obstacle):
+        for ob,idx in zip(self.obstacles, range(len(self.obstacles))):
+            if obstacle.get_id() == ob.get_id():
+                return idx
+        return None
+
+    def get_img(self, scale=10):
+        boundary = self.get_boundary()*scale-scale
+        img = np.ones(np.flip(boundary))
         for el in self.elements:
             geom = el.get_geometry()
-            new_geom = np.zeros(geom.shape,dtype=int)
-            new_geom[:,0] = geom[:,0]
-            new_geom[:,1] = boundary[1]-geom[:,1]            
+            shape = np.zeros(geom.shape,dtype=int)
+            shape[:,0] = geom[:,0]*scale
+            shape[:,1] = boundary[1]-geom[:,1]*scale
             progress = el.is_inprogress()
             if progress:
-                cv2.fillPoly(img,pts=[new_geom],color=0.5)
+                cv2.fillPoly(img,pts=[shape],color=0.5)
             else:
-                cv2.fillPoly(img,pts=[new_geom],color=0)
-            
-        for el in self.elements:
-            elid = el.get_id()
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            loc = el.get_bbox()['max']+5
-            loc[1] = boundary[1]-loc[1]
-            cv2.putText(img,elid,tuple(loc), font, 0.5,(0),2)
+                cv2.fillPoly(img,pts=[shape],color=0)
 
+        for ob in self.obstacles:
+            if ob.is_detected():
+                geom = ob.get_geometry()
+                shape = np.zeros(geom.shape,dtype=int)
+                shape[:,0] = geom[:,0]*scale
+                shape[:,1] = boundary[1]-geom[:,1]*scale
+                cv2.fillPoly(img,pts=[shape],color=0)
+                # cv2.polylines(img,pts=[shape],isClosed=True,color=0,thickness=3)
+
+        for el in self.elements:
+            elid = str(el.get_id())
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            loc_max = (el.get_bbox()['max']*scale)
+            loc_min = (el.get_bbox()['min']*scale)
+            loc = [loc_max[0]+scale, boundary[1]-loc_max[1]-scale]
+            if loc_max[0]>=boundary[0]-scale:
+                loc[0] = loc_min[0]-scale
+            if loc_max[1]>=boundary[1]-scale:
+                loc[1] = boundary[1]-loc_min[1]-scale
+            cv2.putText(img,elid,tuple(loc), font, 0.5,(0),1)
         return img
 
     def show_img(self):
-        img = self.get_img()
+        img = self.get_img(10)
         cv2.imshow('map',img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     def save_img(self, name='map'):
-        img = self.get_img()
+        img = self.get_img(10)
         cv2.imwrite(name+'.png',img*255)
 
-class Element:
-    def __init__(self, id, category, geometry, inprogress=False):
-        self.id = id
-        self.category = None
+class Obstacle:
+    def __init__(self, geometry):
+        self.id = None
         self.geometry = None
-        self.bbox = None
-        self.inprogress = None
-        self.set_category(category)
+        self.detected = False
         self.set_geometry(geometry)
-        self.set_inprogress(inprogress)
     
+    def set_id(self, id):
+        if self.id == None:
+            self.id = id
+        else:
+            print('Can not reassign an id for obstacle with id %s'.format(self.id))
+
     def get_id(self):
         return self.id
-
-    def get_category(self):
-        return self.category
-        
-    def set_category(self, category):
-        self.category = category
 
     def get_geometry(self):
         return self.geometry
@@ -150,11 +179,51 @@ class Element:
     def set_geometry(self, geometry):
         geom = np.array(geometry)
         self.geometry = geom
-        bbox = {'min': np.min(geom, axis=0), 'max':np.max(geom, axis=0)}
-        self.bbox = bbox
+    
+    def is_detected(self):
+        return self.detected
 
+    def set_detected(self, detected):
+        self.detected = detected
+        
     def get_bbox(self):
-        return self.bbox
+        geometry = self.get_geometry()
+        minX = min(geometry[:,0])
+        minY = min(geometry[:,1])
+        maxX = max(geometry[:,0])
+        maxY = max(geometry[:,1])
+        return {'min':np.array([minX,minY]),'max':np.array([maxX,maxY])}
+
+    def get_center(self):
+        bbox = self.get_bbox()
+        centerX = float(bbox['min'][0]+bbox['max'][0])/2
+        centerY = float(bbox['min'][1]+bbox['max'][1])/2
+        return [centerX,centerY]
+    
+    def set_inprogress(self, state):
+        pass
+
+    def clone(self):
+        obstacle = Obstacle(self.geometry)
+        return obstacle
+        
+
+class Element(Obstacle):
+    def __init__(self, name, geometry, inprogress=False):   #Assuing the geometry is a bounding box for now
+        self.id = None
+        self.name = None
+        self.geometry = None
+        self.inprogress = None
+        self.mapped = False
+        self.set_name(name)
+        self.set_geometry(geometry)
+        self.set_inprogress(inprogress)
+
+    def get_name(self):
+        return self.name
+        
+    def set_name(self, name):
+        self.name = name
 
     def is_inprogress(self):
         return self.inprogress
@@ -162,6 +231,9 @@ class Element:
     def set_inprogress(self, state):
         self.inprogress = state
 
+    def clone(self):
+        element = Element(self.name, self.geometry,self.inprogress)
+        return element
 
 if __name__ == "__main__":
     factory = MapFactory()
--- MapNav.py
+++ MapNav.py
@@ -6,18 +6,24 @@ import cv2
 from MapFactory import MapFactory
 
 class MapNav:
-    def __init__(self, map_object, safe_offset=3, observation_dist=200, fov=60):
-        self.map = map_object
+    def __init__(self, map_id, safe_offset=3, sensor_range=10, fov=60, resolution=50):
+        self.map = MapFactory().create_map(map_id)
         self.safe_offset = safe_offset
-        self.observation_dist = observation_dist
+        self.sensor_range = sensor_range
         self.fov = fov
+        self.resolution = resolution
         self.init_config = None
+        self.cur_config = None
+        self.not_mapped = []
+        self.incollision = False
+        self.score = 0
+        self.sim_images = []
         self.reset()
         self.initialize_config()
 
     def reset(self):
-        self.not_mapped = self.map.get_inprogress_ids()
-        self.collision = False
+        self.not_mapped = self.map.get_inprogress()
+        self.incollision = False
         self.score = 0
         self.cur_config = self.init_config
         self.sim_images = []
@@ -28,60 +34,81 @@ class MapNav:
     def end_recording(self):
         try:  
             os.mkdir('./simulation')
-        except OSError:  
+        except OSError:
             files = glob.glob('./simulation/*')
             for f in files:
                 os.remove(f)
 
         for (idx,img) in zip(range(len(self.sim_images)),self.sim_images):
             cv2.imwrite('./simulation/sim_'+str(idx)+'.jpg',img*255)
+        
+    def initialize_config(self):
+        boundary = self.map.get_boundary()
+        map_range = boundary - np.ones(boundary.shape,dtype=int)*2*self.safe_offset
+        for i in range(1000):
+            x_rand = int(random.random()*map_range[0]+self.safe_offset)
+            y_rand = int(random.random()*map_range[1]+self.safe_offset)
+            theta_rand = random.randint(0,7)*45
+            config = Config(x_rand, y_rand, theta_rand)
+            incollision = self.check_collision(config)
+            if not(incollision):
+                self.set_init_config(config)
+                return True
+        raise ValueError('Can not sample a random config in empty space, try to create more empty spaces')
+        return False
 
     def set_init_config(self, config):
         self.init_config = config
         self.cur_config = config
 
-    def get_inflated(self):
-        safe_offset = self.safe_offset
-        inflated = self.map.clone()
-        boundary = inflated.get_boundary()
-        for el in inflated.get_elements():
-            bbox = el.get_bbox()
-            min_x = max(bbox['min'][0] - safe_offset, 0)
-            min_y = max(bbox['min'][1] - safe_offset, 0)
-            max_x = min(bbox['max'][0] + safe_offset, boundary[0])
-            max_y = min(bbox['max'][1] + safe_offset, boundary[1])
-            geom = [(min_x,min_y),(max_x,min_y),(max_x,max_y),(min_x,max_y)]
-            el.set_geometry(geom)
-            el.set_inprogress(False)
+    def inflate(self,element, safe_offset):
+        boundary = self.map.get_boundary()
+        bbox = element.get_bbox()
+        min_x = max(bbox['min'][0] - safe_offset, 0)
+        min_y = max(bbox['min'][1] - safe_offset, 0)
+        max_x = min(bbox['max'][0] + safe_offset, boundary[0])
+        max_y = min(bbox['max'][1] + safe_offset, boundary[1])
+        geom = [(min_x,min_y),(max_x,min_y),(max_x,max_y),(min_x,max_y)]
+        inflated = element.clone()
+        inflated.set_geometry(geom)
+        inflated.set_inprogress(False)
         return inflated
 
     def get_map(self):
         return self.map
 
+    def detect_obstacles(self, config):
+        for o in self.map.get_obstacles():
+            distance = self.get_distance(config, o)
+            if distance<=self.sensor_range:
+                o.set_detected(True)
+            else:
+                o.set_detected(False)
+
     def check_collision(self, config):
         boundary = self.map.get_boundary()
         if config.x >= boundary[0] or config.x <= 0 or config.y >= boundary[1] or config.y <= 0:
-            self.collision = True
+            self.incollision = True
             return True
-        inflated = self.get_inflated()
-        for el in inflated.get_elements():
-            box = el.get_bbox()
-            x_min = box['min'][0]
-            y_min = box['min'][1]
-            x_max = box['max'][0]
-            y_max = box['max'][1]
-            if config.x >= x_min and config.x <= x_max and config.y >= y_min and config.y <= y_max:
-                self.collision = True
-                return True
-        self.collision = False
+        objects = self.map.get_elements()+self.map.get_obstacles()
+        collision = False
+        for o in objects:
+            distance = self.get_distance(config, o)
+            if distance<=self.safe_offset:
+                collision = True
+                break
+        self.incollision = collision
+        return collision
+
+    def set_mapped(self, element):
+        indices = [i for i,el in enumerate(self.not_mapped) if element.get_id() == el.get_id()]
+        if len(indices)>0:
+            del self.not_mapped[indices[0]]
+            return True
         return False
 
-    def set_mapped(self,element_id):
-        self.not_mapped.remove(element_id)
-
-
     def update_observation(self, config):
-        observation_dist = self.observation_dist
+        sensor_range = self.sensor_range
         fov = self.fov
         observed = []
         for el in self.map.get_elements():
@@ -89,12 +116,28 @@ class MapNav:
                 continue
             distance = self.get_distance(config, el)
             angle = self.get_angle(config, el)
-            if distance<observation_dist and angle<(fov/2):
-                elid = el.get_id()
-                if elid in self.not_mapped:
-                    observed.append(elid)
-                    self.set_mapped(elid)
+            if distance<=sensor_range and angle<=(fov/2):
+                mapped = self.set_mapped(el)
+                if mapped:
+                    observed.append(el)
         return observed
+
+    def backproject(self, config, elements):
+        pass
+
+    def elements_in_range(self, config):
+        sensor_range = self.sensor_range
+        fov = self.fov
+        observed = []
+        # First detect elements inside the FOV and range to reduce the backprojection computation
+        for el in self.map.get_elements():
+            distance = self.get_distance(config, el)
+            angle = self.get_angle(config, el)
+            if distance<sensor_range and angle<(fov/2):
+                observed.append(el)
+
+        #backproject the elements to checj visibility
+        zbuffer = np.ones([1,self.resolution])
 
     def get_distance(self, config, element):
         box = element.get_bbox()
@@ -121,21 +164,7 @@ class MapNav:
         heading = math.atan2(x_p-config.x, y_p-config.y)/math.pi*180
         delta = abs(heading-config.theta)
         return 
-        
-    def initialize_config(self):
-        boundary = self.map.get_boundary()
-        map_range = boundary - np.ones(boundary.shape,dtype=int)*2*self.safe_offset
-        for i in range(1000):
-            x_rand = int(random.random()*map_range[0]+self.safe_offset)
-            y_rand = int(random.random()*map_range[1]+self.safe_offset)
-            theta_rand = random.randint(0,7)*45
-            config = Config(x_rand, y_rand, theta_rand)
-            collision = self.check_collision(config)
-            if not(collision):
-                self.set_init_config(config)
-                return True
-        raise ValueError('Can not generate a random init, try to create more empty spaces')
-        return False
+
 
     def apply_action(self, action, render=True):
         # actions can be:
@@ -159,7 +188,8 @@ class MapNav:
             self.cur_config.theta = new_theta % 360
 
         # update collision
-        collision = self.check_collision(self.cur_config)
+        self.detect_obstacles(self.cur_config)
+        incollision = self.check_collision(self.cur_config)
 
         # update observation
         observed = self.update_observation(self.cur_config)
@@ -173,29 +203,32 @@ class MapNav:
 
 
     def render(self):
-        img = self.map.get_img()
+        render_scale = 10
+        img = self.map.get_img(render_scale)
         
-        y_bound = self.map.get_boundary()[1]
+        y_bound = self.map.get_boundary()[1]*render_scale
+        safe_offset = self.safe_offset*render_scale
+        sensor_range = self.sensor_range*render_scale
 
-        loc = (self.cur_config.x,self.cur_config.y)
+        loc = (self.cur_config.x*render_scale,self.cur_config.y*render_scale)
         center = (loc[0], y_bound-loc[1])
         cv2.circle(img,center, 10, (0), -1)
-        cv2.circle(img,center, self.safe_offset, (0), 1)
-        cv2.circle(img,center, self.observation_dist, (0), 1)
+        cv2.circle(img,center, safe_offset, (0), 1)
+        cv2.circle(img,center, sensor_range, (0), 1)
 
 
         head = self.cur_config.theta
-        head_pt = self.get_head_pt(loc, head, self.safe_offset)
+        head_pt = self.get_head_pt(loc, head, safe_offset)
         head_pt = (head_pt[0],y_bound-head_pt[1])
         cv2.line(img, center, head_pt, (0), 2)
 
         st_fov_head = int((head-self.fov/2+360)%360)
-        st_fov_pt = self.get_head_pt(loc, st_fov_head, self.observation_dist)
+        st_fov_pt = self.get_head_pt(loc, st_fov_head, sensor_range)
         st_fov_pt = (st_fov_pt[0],y_bound-st_fov_pt[1])
         cv2.line(img, center, st_fov_pt, (0), 1)
 
         end_fov_head = int((head+self.fov/2)%360)
-        end_fov_pt = self.get_head_pt(loc, end_fov_head, self.observation_dist)
+        end_fov_pt = self.get_head_pt(loc, end_fov_head, sensor_range)
         end_fov_pt = (end_fov_pt[0],y_bound-end_fov_pt[1])
         cv2.line(img, center, end_fov_pt, (0), 1)
 
@@ -206,14 +239,14 @@ class MapNav:
         text = 'Elements to map: ' + str(len(self.not_mapped))
         cv2.putText(img,text ,(50,80), font, 0.7, (0), 2)
 
-        text = 'Collsion: ' + str(self.collision)
+        text = 'Collsion: ' + str(self.incollision)
         cv2.putText(img,text ,(50,110), font, 0.8, (0), 2)
 
         self.sim_images.append(img)
 
 
         cv2.imshow('map',img)
-        cv2.waitKey(5)
+        cv2.waitKey(200)
         # cv2.destroyAllWindows()
 
     def get_head_pt(self, start, head, distance):
@@ -232,41 +265,40 @@ class Config:
         
 
 if __name__ == "__main__":
-    factory = MapFactory()
-    site_map = factory.create_map(1)
-    nav = MapNav(site_map,30,200,90)
-    conf = Config(400,100,0)
+
+    nav = MapNav(map_id=1, safe_offset=3, sensor_range=20, fov=60, resolution=50)
+    conf = Config(40,10,0)
     nav.set_init_config(conf)
 
     nav.start_recording()
 
-    for i in range(100):
+    for i in range(6):
         nav.apply_action(1)
     
     nav.apply_action(2)
     nav.apply_action(2)
 
-    for i in range(300):
+    for i in range(30):
         nav.apply_action(1)
 
     nav.apply_action(3)
 
-    for i in range(50):
+    for i in range(5):
         nav.apply_action(1)
     
     nav.apply_action(3)
     
-    for i in range(500):
+    for i in range(40):
         nav.apply_action(1)
 
     nav.apply_action(2)
     nav.apply_action(2)
 
-    for i in range(400):
+    for i in range(40):
         nav.apply_action(1)
 
     nav.end_recording()
 
 # Convert images to video
-# process = 'ffmpeg -framerate 30 -i simulation/sim_%d.jpg -c:v libx264 sim.avi'
+# process = 'ffmpeg -framerate 10 -i simulation/sim_%d.jpg -c:v libx264 sim.avi'
 
