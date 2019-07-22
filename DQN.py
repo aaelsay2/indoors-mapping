9d6acedd244446bc0bf38a9f31b38b321d0aeb94
trrscld2 <trrscld2@illinois.edu>
2019-07-22 11:29:39 -0500

    updated to dddqn

--- DQN.py
+++ DQN.py
@@ -34,13 +34,26 @@ class DQN(nn.Module):
         self.device = torch.device('cuda')
 
         self.steps = 0
+        hidden_layer = 512
 
+        # Conv Module
         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
+
+        self.feature_extraction = nn.Sequential(
+            self.conv1,
+            self.bn1,
+            nn.ReLU(),
+            self.conv2,
+            self.bn2,
+            nn.ReLU(),
+            self.conv3,
+            self.bn3,
+            nn.ReLU())
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
@@ -49,21 +62,46 @@ class DQN(nn.Module):
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
+
+        # Value Module
+        self.state_fc = nn.Linear(linear_input_size, hidden_layer)
+        self.state_values = nn.Linear(hidden_layer, 1)
+
+        # Advantage Module
+        self.action_fc = nn.Linear(linear_input_size, hidden_layer)
+        self.action_values = nn.Linear(hidden_layer, self.num_actions)
+        
+        # self.head = nn.Linear(linear_input_size, self.num_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
-      # print('Input size: {}'.format(x.size()))
-      x = F.relu(self.bn1(self.conv1(x)))
-      # print('1st cnn size: {}'.format(x.size()))
-      x = F.relu(self.bn2(self.conv2(x)))
-      # print('2nd cnn size: {}'.format(x.size()))
-      x = F.relu(self.bn3(self.conv3(x)))
-      # print('3nd cnn size: {}'.format(x.size()))
-      # print(x.size())
-      # print('Output size: {}'.format(self.head(x.view(x.size(0), -1)).size()))
-      return self.head(x.view(x.size(0), -1))
+
+        normalize_image = False
+
+        if normalize_image:
+            x = x / 255     
+
+        # print('Input size: {}'.format(x.size()))
+        # x = F.relu(self.bn1(self.conv1(x)))
+        # print('1st cnn size: {}'.format(x.size()))
+        # x = F.relu(self.bn2(self.conv2(x)))
+        # print('2nd cnn size: {}'.format(x.size()))
+        # x = F.relu(self.bn3(self.conv3(x)))
+        # print('3nd cnn size: {}'.format(x.size()))
+        # print(x.size())
+        # print('Output size: {}'.format(self.head(x.view(x.size(0), -1)).size()))
+
+        x = self.feature_extraction(x).view(x.size(0), -1)
+
+        # Through Advantage Module
+        action_v = self.action_values(self.action_fc(x))
+
+        # Through Value Module
+        state_v = self.state_values(self.state_fc(x))
+
+        # return self.head(x.view(x.size(0), -1))
+        return state_v + action_v - action_v.mean(dim=1, keepdim=True)
 
     def _get_eps(self):
         if(self.steps > EPS_STEP_END):
